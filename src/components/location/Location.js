9232d505330539c0499524c4d82fa58059f40e0b
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-05-28 10:57:41 -0500

    Book 1: Complete

---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Location.css";
+
+export const Location = () => (
+  <section className="location">
+    <h3 className="location__name">NK West</h3>
+    <div className="location__address">8210 Boling St</div>
+  </section>
+);
